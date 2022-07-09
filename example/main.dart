e16302ccc6c5f4a090c0f8f5bfee4d9a6e1ecf1a
Vonarian <68429735+Vonarian@users.noreply.github.com>
2022-07-09 07:28:05 +0430

    Add listenable devices

--- example/main.dart
+++ example/main.dart
@@ -3,7 +3,6 @@ import 'package:openrgb/openrgb.dart';
 
 Future<void> main() async {
   final oRgb = await OpenRGBClient.connect();
-  List<RGBController> controllers = await oRgb.getAllControllers();
   Color color = const Color.rgb(255, 255, 255);
   await oRgb.updateSingleLed(2, 0, color);
 }
